e212cee8f78242b45564ffb71e0cf9409e860f90
Miguel Augusto Baricelli <baricellimiguel@gmail.com>
2026-06-18 01:04:04 -0300

    added Producer

---
+++ MarketDataCentralizer.Infrastructure.RabbitMq/RabbitMqProducer.cs
@@ -0,0 +1,61 @@
+﻿using MarketDataCentralizer.Infrastructure.RabbitMq.Connection;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+public class RabbitMqProducer
+{
+    private readonly IRabbitMqConnection _connection;
+    private readonly ILogger<RabbitMqProducer> _logger;
+
+    public RabbitMqProducer(IRabbitMqConnection connection, ILogger<RabbitMqProducer> logger )
+    {
+        _connection = connection;
+        _logger = logger;
+    }
+
+    public async Task PublishAsync<T>(
+        string queueName,
+        T message)
+    {
+
+        try
+        {
+            var conn = await _connection.GetConnectionAsync();
+
+            await using var channel = await conn.CreateChannelAsync();
+
+            var arguments = new Dictionary<string, object?>
+            {
+                 { "x-message-ttl", 86400000 } 
+            };
+
+            await channel.QueueDeclareAsync(
+                queue: queueName,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments);
+
+            var body = Encoding.UTF8.GetBytes(
+                JsonSerializer.Serialize(message));
+
+            var properties = new BasicProperties { Persistent = true };
+
+            await channel.BasicPublishAsync(
+                exchange: string.Empty,
+                routingKey: queueName,
+                mandatory: false,
+                basicProperties: properties,
+                body: body);
+
+            _logger.LogInformation("[{Class}] [{Method}] Mensagem publicada na fila {Queue}", nameof(RabbitMqProducer), nameof(PublishAsync), queueName);
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+       
+}
---
+++ MarketDataCentralizer/Controllers/V1/RabbitMq/RabbitMqController.cs
@@ -0,0 +1,39 @@
+﻿using MarketDataCentralizer.Infrastructure.RabbitMq.Messages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketDataCentralizer.Controllers.V1.RabbitMq
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class RabbitMqController : ControllerBase
+    {
+        private readonly RabbitMqProducer _producer;
+
+        public RabbitMqController(
+            RabbitMqProducer producer)
+        {
+            _producer = producer;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Publish()
+        {
+            var evt = new MarketSituationMessageEvent
+            {
+               Current_Status = "Fechado",
+                Local_Close = "17:00",
+                Local_Open = "09:30",
+                Market_Type = "Ações",
+                Notes = "Mercado fechado",
+                Primary_Exchanges = "B3",
+                Region = "Brasil"
+            };
+
+            await _producer.PublishAsync(
+                "",
+                evt);
+
+            return Ok("Mensagem enviada");
+        }
+    }
+}
